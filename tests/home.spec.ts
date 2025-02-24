b244112811d5e349e378a5175d1a0d68452b569d
naopeke <naopeke@protonmail.com>
2025-02-24 13:15:06 +0100

    2-6

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,21 +1,35 @@
 import { test, expect } from "@playwright/test";
 
-test("Home page", async ({ page }) => {
-  await page.goto("https://practicesoftwaretesting.com/");
-  // ensure the sign-in link s present
-  await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
-  // chack the title of the page
-  await expect(page).toHaveTitle(
-    "Practice Software Testing - Toolshop - v.5.0"
-  );
-  // check the count of items displayed
-  const productGrid = page.locator(".col-md-9");
-  await expect(productGrid.getByRole("link")).toHaveCount(9);
-  expect(await productGrid.getByRole("link").count()).toBe(9);
+test.describe("Home page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/");
+  });
 
-  // search for thor hammer and check the result
-  await page.getByTestId("search-query").fill("Thor Hammer");
-  await page.getByTestId("search-submit").click();
-  await expect(productGrid.getByRole("link")).toHaveCount(1);
-  await expect(page.getByAltText("Thor Hammer")).toBeVisible();
+  test("check sign in", async ({ page }) => {
+    // ensure the sign-in link s present
+    await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
+  });
+
+  test("validate page title", async ({ page }) => {
+    // chack the title of the page
+    await expect(page).toHaveTitle(
+      "Practice Software Testing - Toolshop - v.5.0"
+    );
+  });
+
+  test("grid loads with 9 items", async ({ page }) => {
+    // check the count of items displayed
+    const productGrid = page.locator(".col-md-9");
+    await expect(productGrid.getByRole("link")).toHaveCount(9);
+    expect(await productGrid.getByRole("link").count()).toBe(9);
+  });
+
+  test("search for thor hammer", async ({ page }) => {
+    // search for thor hammer and check the result
+    const productGrid = page.locator(".col-md-9");
+    await page.getByTestId("search-query").fill("Thor Hammer");
+    await page.getByTestId("search-submit").click();
+    await expect(productGrid.getByRole("link")).toHaveCount(1);
+    await expect(page.getByAltText("Thor Hammer")).toBeVisible();
+  });
 });
