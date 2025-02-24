43a7c95c722d99a3db003b4fb66fd45ba89c1edc
naopeke <naopeke@protonmail.com>
2025-02-24 16:09:45 +0100

    challange

--- tests/challange.spec.ts
+++ tests/challange.spec.ts
@@ -37,7 +37,35 @@ test.describe("Check out", async () => {
     await expect(page.getByTestId("payment-success-message")).toHaveText(
       "Payment was successful"
     );
+
+    headless
+      ? await test.step("visual test", async () => {
+          await expect(page).toHaveScreenshot("checkout.png", {
+            mask: [page.getByTitle("Practice Software Testing")],
+          });
+        })
+      : console.log("Running in Headed mode, no screenshot comparison");
   });
 });
 
-test.describe("Api", async () => {});
+test.describe("Api", async () => {
+  test("GET /products/{id}", async ({ request }) => {
+    const apiUrl = "https://api.practicesoftwaretesting.com";
+    const productResponse = await request.get(
+      apiUrl + "/products/search?q=thor%20hammer"
+    );
+    expect(productResponse.status()).toBe(200);
+    const productBody = await productResponse.json();
+    const productId = productBody.data[0].id;
+
+    const response = await request.get(apiUrl + "/products" + productId);
+    expect(response.status()).toBe(200);
+    const body = await response.json();
+
+    expect(body.in_stock).toBe(true);
+    expect(body.is_location_offer).toBe(false);
+    expect(body.is_rental).toBe(false);
+    expect(body.name).toBe("Thor Hammer");
+    expect(body.price).toBe(11.14);
+  });
+});
